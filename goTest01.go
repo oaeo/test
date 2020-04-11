007f3445f1287af0e81462ab1e45cbbc91cde182
Lin <wnag@163.com>
2020-04-12 00:27:54 +0800

    第一次使用创建go文件
    第一次使用创建go文件

---
+++ goTest01.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func printArray( arr [5]int)  {
+	arr[0] = 100
+	for i,v:= range arr{
+		fmt.Println(i,v)
+	}
+}
+
+func main()  {
+	var arr1 [5]int
+	arr2 := [3]int{1 ,3,5}
+	arr3 := [...]int{2,3,4,5,6}
+
+	fmt.Println(arr1,arr2,arr3)
+
+	for i,v:= range arr3{
+		fmt.Println(i,v)
+	}
+
+	printArray(arr3)
+}
